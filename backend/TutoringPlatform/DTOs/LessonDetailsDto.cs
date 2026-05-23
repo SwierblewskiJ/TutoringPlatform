1ad148461b40965490e360cbf10967860f37d27a
matixi9 <290216@student.pwr.edu.pl>
2026-05-24 01:12:40 +0200

    feat: add LessonDetailsDto for dashboard overview

---
+++ backend/TutoringPlatform/DTOs/LessonDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TutoringPlatform.DTOs;
+
+public class LessonDetailsDto
+{
+    public int Id { get; set; }
+    public required DateTime StartTime { get; set; }
+    public required string Status { get; set; }
+    public required bool IsRecurring { get; set; }
+    public int? RemainingLessons { get; set; }
+    public required string AdTitle { get; set; }
+    
+    //dla ucznia: imie nauczyciela
+    //dla nauczyciela: imie ucznia
+    public required string RelatedUser { get; set; }
+}
