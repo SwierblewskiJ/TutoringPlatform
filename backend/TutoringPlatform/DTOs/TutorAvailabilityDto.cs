d30190575858f0508566e0b752e9f77937ef7b18
matixi9 <290216@student.pwr.edu.pl>
2026-05-23 23:36:39 +0200

    feat: add DTOs for tutor availability management

---
+++ backend/TutoringPlatform/DTOs/TutorAvailabilityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TutoringPlatform.DTOs;
+
+public class TutorAvailabilityDto
+{
+    public int Id { get; set; }
+    public required string DayOfWeek { get; set; }
+    public required TimeSpan StartTime { get; set; }
+    public required TimeSpan EndTime { get; set; }
+}
