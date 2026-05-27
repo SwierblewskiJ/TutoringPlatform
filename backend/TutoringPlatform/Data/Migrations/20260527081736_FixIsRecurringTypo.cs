fe94e474ec2fb4adc8d2ea5829057bad793a7625
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-05-27 12:45:27 +0200

    chore: new migrations

---
+++ backend/TutoringPlatform/Data/Migrations/20260527081736_FixIsRecurringTypo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutoringPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixIsRecurringTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
